93b6e22bf406e315087183b8a638b5a51a61a0c4
Pedro <pedrocauaggn@gmail.com>
2025-10-14 13:44:23 -0300

    feat: criação da navbar e perfil do responsável.

--- client/navigation/PrivateNavigator.js
+++ client/navigation/PrivateNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/Home/HomeScreen';
+import PerfilResponsavelScreen from '../screens/Perfil/PerfilResponsavelScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -8,6 +9,7 @@ export default function PrivateNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="PerfilResponsavel" component={PerfilResponsavelScreen} />
     </Stack.Navigator>
   );
 }
